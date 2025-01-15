759c272f1ac4003a2401b113ab4f16e050ef77ba
David Nádvorník <david@nadvornik.cz>
2025-01-15 18:35:12 +0100

    init

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+	"github.com/pocketbase/pocketbase/tools/subscriptions"
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	app := pocketbase.New()
+	// Add your custom routes or hooks here
+
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Dashboard
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
+	})
+
+	app.OnRecordAuthWithOAuth2Request("users").BindFunc(func(e *core.RecordAuthWithOAuth2RequestEvent) error {
+
+		if e.ProviderName == "discord" {
+			app.Logger().Debug("Discord auth event", "eventData", e)
+		}
+		return e.Next()
+	})
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
+func notify(app core.App, subscription string, data any) error {
+	rawData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	message := subscriptions.Message{
+		Name: subscription,
+		Data: rawData,
+	}
+
+	group := new(errgroup.Group)
+
+	chunks := app.SubscriptionsBroker().ChunkedClients(300)
+
+	for _, chunk := range chunks {
+		group.Go(func() error {
+			for _, client := range chunk {
+				if !client.HasSubscription(subscription) {
+					continue
+				}
+
+				client.Send(message)
+			}
+
+			return nil
+		})
+	}
+
+	return group.Wait()
+}
---
+++ migrations/1736644268_updated_users.go
@@ -0,0 +1,45 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(8, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text2234282670",
+			"max": 0,
+			"min": 0,
+			"name": "discordId",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": false,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("text2234282670")
+
+		return app.Save(collection)
+	})
+}
